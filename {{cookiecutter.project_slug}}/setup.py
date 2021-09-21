bff526a51aa3d0b95d4290ec65af85611c2d49c2
myroshnychenkm2 <mmyros@gmail.com>
2021-09-21 16:24:31 -0400

    add contents of requirements.txt to setup.py

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -10,7 +10,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
+
 requirements = [{%- if cookiecutter.command_line_interface|lower == 'click' %}'Click>=7.0',{%- endif %} ]
+
+with open('requirements.txt') as f:
+    requirements += f.read().splitlines()
 
 test_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest>=3',{%- endif %} ]
 
